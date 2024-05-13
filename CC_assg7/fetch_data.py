cb1bfdcdfae8d87a91594be8e37cd4ea1fe8e188
Pradeep09102 <149702470+Pradeep09102@users.noreply.github.com>
2024-05-13 18:49:23 -0500

    Delete CC_assg7 directory

--- CC_assg7/fetch_data.py
+++
@@ -1,5 +0,0 @@
-import requests
-api_url = 'https://api.covidactnow.org/v2/states.json?apiKey=254b42b4fac144718664aca60b349b51'
-response = requests.get(api_url)
-data = response.json()
-print(data)
